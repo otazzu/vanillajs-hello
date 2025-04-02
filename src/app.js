05dedd46ae2c6e72e45054af0a88b2b042d102cc
otazzu <srotazu@gmail.com>
2025-04-02 19:14:06 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  let frase = document.getElementById("excuse")
+
+  let quien = ['El perro', 'La awela', 'El portero', 'Un pajaro'];
+  let loHizo = ['comió', 'meó', 'estampó', 'rompió'];
+  let que = ['mi codigo', 'mi cama', 'el coche'];
+  let cuando = ['antes de clase', 'mientras dormia', 'mientras hacia ejercico', 'en la cena', 'mientras rezaba'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  frase.innerHTML = `${quien[random(quien.length)]} ${loHizo[random(loHizo.length)]} ${que[random(que.length)]} ${cuando[random(cuando.length)]}`
+
 };
+
+const random = (n) => {
+  let num = Math.floor(Math.random() * n)
+  console.log(num)
+  return num
+}
